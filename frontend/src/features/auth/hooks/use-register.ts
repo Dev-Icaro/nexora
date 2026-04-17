49b0b99057641aceeb10d9a3f2c20bb0eaed41f2
icaro.kiiler.melo <icarokiilermelo@gmail.com>
2026-04-17 19:56:22 -0300

    feature: Add login after register

--- frontend/src/features/auth/hooks/use-register.ts
+++ frontend/src/features/auth/hooks/use-register.ts
@@ -5,6 +5,7 @@ import { getApiErrorMessage } from '@/shared/lib/utils';
 
 import { REGISTER_MUTATION } from '../api/auth.mutations';
 import type { RegisterRequest, RegisterResponse } from '../api/auth.types';
+import { useLogin } from './use-login';
 
 type UseRegisterResult = {
   register: (input: RegisterRequest) => Promise<void>;
@@ -15,11 +16,16 @@ type UseRegisterResult = {
 
 export function useRegister(): UseRegisterResult {
   const [registerMutation, { loading, error, data }] = useMutation<RegisterResponse>(REGISTER_MUTATION);
+  const { login } = useLogin();
 
   const errorMessage = useMemo(() => getApiErrorMessage(error, data), [data, error]);
 
   const register = async (input: RegisterRequest) => {
-    await registerMutation({ variables: { registerRequest: input } });
+    const { data: response } = await registerMutation({ variables: { registerRequest: input } });
+
+    if (response?.register.success) {
+      await login({ email: input.email, password: input.password });
+    }
   };
 
   return { register, loading, error: errorMessage, data };
